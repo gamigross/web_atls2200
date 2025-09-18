902a2fca5779eb9d0a3e55ca399c9c3a4dbf3f0b
Maxwell Julian <89419262+gamigross@users.noreply.github.com>
2025-09-18 13:46:58 -0600

    WGUH

---
+++ wa5/script.js
@@ -0,0 +1,8 @@
+const navToggle = document.querySelector('nav-toggle');
+const navMenu = document.querySelector('.nav-menu'); 
+
+function showMenu() {
+
+}
+
+navToggle.addEventListener('click', showMenu);
